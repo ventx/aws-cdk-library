b9001d73d1c81101564681c0395301d0397dfa45
Raphael <r.schnaitl@gmail.com>
2019-07-04 14:27:43 +0200

    added workaround for pypi not liking the md files

---
+++ PyPi_Documentation.md
@@ -0,0 +1,3 @@
+# Ventx AWS CDK Library
+
+Check out the Documentation on [github](https://github.com/ventx/aws-cdk-library).
--- upload.sh
+++ upload.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 npm i
+rm -rf dist/
+
+mv README.md README.md.backup
+cp PyPi_Documentation.md README.md
+
 npm run build
 npm run package
-
 python3.7 -m twine upload ./dist/python/*whl
+
+rm README.md
+mv README.md.backup README.md
+
+npm run build
+npm run package
 npm publish --access public ./dist/js/*.jsii.tgz
